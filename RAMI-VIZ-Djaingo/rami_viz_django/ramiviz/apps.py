bab74655b6dcec42dbefb9a62a1ba9af85855420
Salman Javed <71730474+javedsalman@users.noreply.github.com>
2025-05-26 00:18:57 +0200

    Delete RAMI-VIZ-Djaingo directory

--- RAMI-VIZ-Djaingo/rami_viz_django/ramiviz/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class RamivizConfig(AppConfig):
-    name = 'ramiviz'
--- RAMI-VIZ-Djaingo/rami_viz_django/ramiviz/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('upload/', views.upload_file, name='upload_file'),
-]
